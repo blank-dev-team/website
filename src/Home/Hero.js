f8329f269ab53baea094480c192c4819867c0cd5
SeunSarumi <seunsarumi@gmail.com>
2023-04-26 07:48:12 +0100

    Hero built

---
+++ src/Home/Hero.js
@@ -0,0 +1,31 @@
+import "./Hero.css";
+import Nav from "./Nav";
+import HeroImg from "../Images/Hero.png";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <Nav></Nav>
+      <div className="hero-container">
+        <div className="hero-container__text-box">
+          <h1 className="hero-container__text-box-heading-primary">
+            Hello to <span className="fig">Convenience.</span>All your cards in
+            one place!
+          </h1>
+          <h3 className="hero-container__text-box-heading-secondary">
+            Now you can put all your cards in one card.Never have to fumble
+            between cards again when you need to pay at a terminal
+          </h3>
+          <a className="cta-btn" href="#">
+            Get Started
+          </a>
+        </div>
+        <div className="hero-container__img-box">
+          <img className="hero-img" src={HeroImg}></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import "./Home.css";
 import Nav from "./Nav";
+import Hero from "./Hero";
 
 function Home() {
   return (
     <div className="home">
-      <Nav></Nav>
+      {/* <Nav></Nav> */}
+      <Hero></Hero>
     </div>
   );
 }
