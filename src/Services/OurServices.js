1696bc12c8d3fe0623fe2e8e272e6181fdf7278e
SeunSarumi <seunsarumi@gmail.com>
2023-05-30 12:13:16 +0100

    OurServices before material UI implementation

---
+++ src/Services/OurServices.js
@@ -0,0 +1,16 @@
+import "./OurServices.css";
+
+function OurServices() {
+  return (
+    <div className="ourservices">
+      <div className="text-container">
+        <h1 className="ourservices-header">Our Services</h1>
+        <p className="ourservices-text">
+          With the Blank app you get to access a whole lot of amazing services
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default OurServices;
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,12 +1,14 @@
 import Nav from "../Home/Nav";
 import ServicesHero from "./ServicesHero";
 import Footer from "../Home/Footer";
+import OurServices from "./OurServices";
 
 function Services() {
   return (
     <div className="services">
       {/* <Nav></Nav> */}
       <ServicesHero></ServicesHero>
+      <OurServices></OurServices>
       <Footer></Footer>
     </div>
   );
