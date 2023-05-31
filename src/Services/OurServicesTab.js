0208e7444d82a35c3375e70c6829e140a66b7afa
SeunSarumi <seunsarumi@gmail.com>
2023-05-31 22:17:12 +0100

    lottie fix before logic

--- src/Services/OurServicesTab.js
+++ src/Services/OurServicesTab.js
@@ -1,19 +1,34 @@
 import "./OurServicesTab.css";
 import Addcard from "../Images/white-request-card.svg";
-import Truck from "../Images/Transport 2.json";
+import Lottie from "lottie-react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import Truck from "../Images/Truck3.gif";
+import Transport from "../Images/Transport2.json";
+
+import React from "react";
+// import { useState } from "react";
 
 function OurServicesTab() {
   return (
     <div className="tab-container">
       <div className="services-button-container">
         <div className="service-button-contained">
-          <img className="service-btn" src={Addcard}></img>
+          <img
+            className="service-btn"
+            // {...(state ? "service-btn" : "service-btn-show")}
+            src={Addcard}
+          ></img>
           <p className="service-btn-text">Request Card</p>
         </div>
       </div>
       <div className="Service-content">
-        <div>
-          <img src={Truck}></img>
+        <div className="player-container">
+          <Player className="player" src={Transport} loop autoplay speed={10} />
+          <p className="player-text">
+            Our Request Card service is designed to make it easy for you to get
+            the payment card you need, exactly when you need it. Simply provide
+            us with your information, and we'll process your request in no time.
+          </p>
         </div>
         <div></div>
       </div>
