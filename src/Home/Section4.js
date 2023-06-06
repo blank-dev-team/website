eac4e7dbd6a827966ea0083cc011f5cb75131175
SeunSarumi <seunsarumi@gmail.com>
2023-06-07 00:04:02 +0100

    minor mobile adjustments

--- src/Home/Section4.js
+++ src/Home/Section4.js
@@ -5,7 +5,7 @@ function Section4() {
   return (
     <div className="section-4">
       <div className="container-1">
-        <div className="grid grid--2-cols">
+        <div className="grid grid--2-cols grid-4">
           <div className="container__text-box">
             <h1 className="text-box__header-primary">
               The Only Card You'll Ever Need
