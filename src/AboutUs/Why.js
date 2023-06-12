1034b74f751f7f0b64b9fee8d52516f63f0691a0
SeunSarumi <seunsarumi@gmail.com>
2023-06-12 16:45:58 +0100

    finished style desktopo

--- src/AboutUs/Why.js
+++ src/AboutUs/Why.js
@@ -1,6 +1,11 @@
 import "./Why.css";
 import Phone2 from "../Images/Phone2.png";
 import SuperFast from "../Images/SuperFast.png";
+import AllOne from "../Images/AllOne.png";
+import DataTransparency from "../Images/DataTrans.png";
+import Secure from "../Images/Secure.png";
+import Easy from "../Images/Easy.png";
+import Subscription from "../Images/Sub.png";
 
 function Why() {
   return (
@@ -19,7 +24,8 @@ function Why() {
             <div className="container-text">
               <h1>Super Fast</h1>
               <p>
-                Transactions are carried out with ease and lightening speed.
+                Transactions are carried out with<br></br> ease and lightening
+                speed.
               </p>
             </div>
             <div className="container-img">
@@ -30,31 +36,67 @@ function Why() {
             <div className="container-text">
               <h1>All-in-One</h1>
               <p>
-                The App has everything. From bills, to airtime, wallet and much
-                more.
+                The App has everything. From bills,<br></br> to airtime, wallet
+                and much more.
               </p>
             </div>
             <div className="container-img">
-              <img className="container-img-img" src={SuperFast} />
+              <img className="container-img-img" src={AllOne} />
             </div>
           </div>
           <div className="text-choose-container-3">
             <div className="container-text">
               <h1>Data Transparency</h1>
               <p>
-                We don't store user sensitive data. Your financial information
-                is safe.
+                We don't store user sensitive data.<br></br> Your financial
+                information is safe.
               </p>
             </div>
             <div className="container-img">
-              <img className="container-img-img" src={SuperFast} />
+              <img className="container-img-img" src={DataTransparency} />
             </div>
           </div>
         </div>
         <div>
           <img className="phoneimg" src={Phone2} />
         </div>
-        <div>hello</div>
+        <div className="grid-3">
+          <div className="text-choose-container-4">
+            <div className="container-img-2">
+              <img className="container-img-img-2" src={Secure} />
+            </div>
+            <div className="container-text-2">
+              <h1>Very Secure</h1>
+              <p>
+                Security protocols protect all<br></br> transactions on the app.
+              </p>
+            </div>
+          </div>
+          <div className="text-choose-container-5">
+            <div className="container-img-2">
+              <img className="container-img-img-2" src={Easy} />
+            </div>
+            <div className="container-text-2">
+              <h1>Easy to Use</h1>
+              <p>
+                The Blank app is very simple to use,<br></br> interface is
+                sleek.
+              </p>
+            </div>
+          </div>
+          <div className="text-choose-container-6">
+            <div className="container-img-2">
+              <img className="container-img-img-2" src={Subscription} />
+            </div>
+            <div className="container-text-2">
+              <h1>No Subscription</h1>
+              <p>
+                You don't have to pay a monthly<br></br> subscription. Just
+                order the card.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
