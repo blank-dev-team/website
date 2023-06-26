40cfe2de29057058797ae3032032b451187f79c4
SeunSarumi <seunsarumi@gmail.com>
2023-06-26 23:21:39 +0100

    services fix

--- src/Services/ServicesHero.js
+++ src/Services/ServicesHero.js
@@ -26,6 +26,7 @@ function ServicesHero() {
             <button onClick={() => setOpenModal(true)} className="Modal-btn">
               Get Started
             </button>
+            <Modal open={openModal} onClose={() => setOpenModal(false)} />
           </div>
           <div className="hero-container__img-box">
             <img className="img-box-img" src={Services}></img>
